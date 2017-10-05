66751896758990a4b8f6c1350d8b5afb64131469
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 07:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -112,11 +112,11 @@ function endGame() {
 
 function moveDodger(e) {
   e.preventDefault()
-   if(e.which == LEFT_ARROW) {
-     moveDodgerLeft()
-   } else if(e.which == RIGHT_ARROW) {
-     moveDodgerRight()
-   }
+  if(e.which == LEFT_ARROW) {
+    moveDodgerLeft()
+  } else if(e.which == RIGHT_ARROW) {
+    moveDodgerRight()
+  }
 }
 
 function moveDodgerLeft() {
