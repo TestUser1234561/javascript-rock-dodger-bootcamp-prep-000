59b253b54652757892dcef145ad5de1ff1ef15cb
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 08:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function createRock(x) {
 
     rock.style.top = `${top += 1}px`
 
-    if(top !> 140) {
+    if(top !> 400) {
         window.requestAnimationFrame(moveRock)
     } else {
       rock.remove()
